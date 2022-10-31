5b45b44250a6dbed70265ac5a034a2e71490a1b6
xeceryn <adityamusthofa67@gmail.com>
2022-10-31 10:28:14 +0700

    feat(slicing login): slicing logo, and form, wait reusable component and fix it

--- src/navigations/index.navigation.js
+++ src/navigations/index.navigation.js
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import IntroScreen from '../screens/introscreen/index.introscreen';
+import LoginScreen from '../screens/loginscreen/index.loginscreen';
 const Stack = createNativeStackNavigator();
 const Bottom = createBottomTabNavigator();
 
@@ -13,7 +14,7 @@ const Navigation = () => {
         screenOptions={{
           headerShown: false,
         }}>
-        <Stack.Screen name="Intro" component={IntroScreen} />
+        <Stack.Screen name="Intro" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/utils/images.utils.js
+++ src/utils/images.utils.js
@@ -4,8 +4,7 @@ import Intro3Bg from '../assets/images/intro3bg.png';
 import Presentation from '../assets/images/Presentation.png';
 import GradHat from '../assets/images/GradHat.png';
 import Bag from '../assets/images/Bag.png';
-import arrowRight from '../assets/images/arrow-right.png';
-import arrowLeft from '../assets/images/arrow-left.png';
+import LoginBg from '../assets/images/loginbg.png';
 
 const images = {
   Intro1Bg,
@@ -14,8 +13,7 @@ const images = {
   Presentation,
   GradHat,
   Bag,
-  arrowRight,
-  arrowLeft,
+  LoginBg,
 };
 
 export default function getImageByName(key) {
